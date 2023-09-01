10fd70a3758cb8dc91bc19943834f53b6b9e92ca
Bohdan Local <tokaretcbogdan@gmail.com>
2023-09-01 23:33:37 +0300

    Add 2 lab

---
+++ DotNET_Lab2/Program.cs
@@ -0,0 +1,10 @@
+﻿using stringTransform;
+
+string Test ="I am Bogdan and I am working on my second lab";
+Console.WriteLine(Test);
+Console.WriteLine(Transform.ToUpper(Test));
+Console.WriteLine(Transform.CountSpaces(Test));
+Console.ReadKey();
+
+
+
---
+++ stringTransform/Transform.cs
@@ -0,0 +1,33 @@
+﻿namespace stringTransform
+{
+    public class Transform
+    {
+        public static string ToUpper(string s)
+        {
+            char[] chars = s.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (char.IsLetter(chars[i]) && char.IsLower(chars[i])) 
+                {
+                    chars[i] = char.ToUpper(chars[i]);
+                }
+            }
+            return new string(chars);
+        }
+        public static int CountSpaces(string s)
+        {
+            int count = 0;
+
+            foreach (char c in s)
+            {
+                if (char.IsWhiteSpace(c))
+                { 
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
