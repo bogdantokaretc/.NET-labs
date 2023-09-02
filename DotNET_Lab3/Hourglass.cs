655529d44fe169fb87d844d79ff18dc13fcdf71f
bogdantokaretc <58222110+bogdantokaretc@users.noreply.github.com>
2023-09-02 22:50:32 +0300

    Undone (#1)
    * Third Lab
    
    * Finish 3 lab

---
+++ DotNET_Lab3/Hourglass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DotNET_Lab3
+{
+    public class Hourglass
+    {
+
+        private double a;
+        private double b;
+
+        public Hourglass(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public Hourglass() : this(0, 0) 
+        {
+
+        }
+        public void Draw(Canvas canvas)
+        {
+            canvas.Children.Clear();
+            Point p0 = new Point(canvas.Width / 2, canvas.Height / 2);
+
+            Point p1 = new Point(p0.X - a / 2, p0.Y - Math.Sqrt(3) * a / 2);
+            Point p2 = new Point(p0.X + a / 2, p0.Y - Math.Sqrt(3) * a / 2);
+
+            Point p3 = new Point(p0.X - a / 2, p0.Y + Math.Sqrt(3) * a / 2);
+            Point p4 = new Point(p0.X + a / 2, p0.Y + Math.Sqrt(3) * a / 2);
+
+
+            Polygon upperTriangle = new Polygon
+            {
+                Points = new PointCollection { p0, p1, p2 },
+                Fill = Brushes.Blue
+            };
+            Polygon lowerTriangle = new Polygon
+            {
+                Points = new PointCollection { p0, p3, p4 },
+                Fill = Brushes.Yellow
+            };
+
+            canvas.Children.Add(upperTriangle);
+            canvas.Children.Add(lowerTriangle);
+        }
+    }
+    
+}
---
+++ DotNET_Lab3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DotNET_Lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CreateSandglassButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (double.TryParse(A.Text, out double a) && double.TryParse(B.Text, out double b))
+            {
+                Hourglass hourglass = new Hourglass(a, b);
+                hourglass.Draw(MainCanvas);
+            }
+            else
+            {
+                MessageBox.Show("Invalid value for A");
+            }
+        }
+        private void A_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (double.TryParse(A.Text, out double a))
+            {
+                B.Text = (Math.Sqrt(3) * a).ToString("f1");
+            }
+        }
+    }
+}
