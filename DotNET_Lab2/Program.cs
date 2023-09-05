57e348a51626bf95286bde0c0be3c041c0f78bd2
Bohdan Local <tokaretcbogdan@gmail.com>
2023-09-05 13:47:43 +0300

    Update second lab

--- DotNET_Lab2/Program.cs
+++ DotNET_Lab2/Program.cs
@@ -3,7 +3,7 @@
 string Test ="I am Bogdan and I am working on my second lab";
 Console.WriteLine(Test);
 Console.WriteLine(Transform.ToUpper(Test));
-Console.WriteLine(Transform.CountSpaces(Test));
+Console.WriteLine("Number of white spaces: {0}",Transform.CountSpaces(Test));
 Console.ReadKey();
 
 
